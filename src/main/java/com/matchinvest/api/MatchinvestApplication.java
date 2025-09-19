1b4c7b30638f9aa09d2b565ae7902597a9e1e477
Natan-TI <natan201125@hotmail.com>
2025-09-19 15:46:43 -0300

    Project created

---
+++ src/main/java/com/matchinvest/api/MatchinvestApplication.java
@@ -0,0 +1,13 @@
+package com.matchinvest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchinvestApplication.class, args);
+	}
+
+}
